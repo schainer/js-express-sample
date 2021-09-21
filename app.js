7f8ee82351f000ef8a3fbbbb6887b033814f2dd8
kjunha <kjunha77@gmail.com>
2021-09-21 22:47:17 +0900

    add static middleware

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ const app = express();
 
 
 app.use(morgan('dev'));
+// Express Static Middleware
+app.use('/static', express.static(path.join(__dirname, 'public')));
 // app.use(morgan('combined'));
 app.use(cookieParser()); // res.cookie(key, value, option) && clearCookie / req.cookies => get Cookie
 app.use(express.json()); // Content-Type: application/json parser
 app.use(express.urlencoded({ extended: true })); // Content-Type: multipart/formData parser, file: multer
+
 
 // express get-set
 app.set('port', process.env.PORT || 3000);
