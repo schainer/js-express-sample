22c915c9ab474bd0ae1980925eea74c9c5adef34
kjunha <kjunha77@gmail.com>
2021-09-25 23:50:18 +0900

    create router setup

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,14 @@
 'use strict';
 const routeGroup = require('express').Router;
 module.exports = (router) => {
-  router.use('/default', () => {
-    const defaultRouter = routeGroup({mergeParams: true})
-    defaultRouter.route('/').get((req, res) => {
-      return res.send('default route!')
-    })
-  })
-  return router
-}
+  // Default Routes
+  const defaultRouter = () => {
+    const router = routeGroup({ mergeParams: true });
+    router.route('/').get((req, res) => {
+      return res.send('default route!');
+    });
+    return router;
+  };
+  router.use('/default', defaultRouter());
+  return router;
+};
