6a7d406bc4664b2e55a8aef4ae2f567485612097
kjunha <kjunha77@gmail.com>
2021-09-26 23:13:09 +0900

    test ORM

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,9 +1,31 @@
 'use strict';
 const Controller = require('./Controller');
+const User = require('../../models/User');
 
 class UserController extends Controller {
   static getUser(req, res, next) {
     res.send('hello Controller!');
+  }
+
+  static async addUser(req, res, next) {
+    try {
+      const user = await User.create({
+        name: req.body.name,
+        age: req.body.age
+      });
+      res.json(user);
+    } catch (e) {
+      throw new Error('create user');
+    }
+  }
+
+  static async getUserById(req, res, next) {
+    try {
+      const user = await User.findByPk(req.params.id)
+      res.json(user)
+    } catch (e) {
+      throw new Error('get single user')
+    }
   }
 }
 
--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,15 @@
 'use strict';
 const routeGroup = require('express').Router;
-const UserController = require('../app/controllers/UserController')
+const UserController = require('../app/controllers/UserController');
 module.exports = (router) => {
   // Default Routes
   const userRouter = () => {
     const router = routeGroup({ mergeParams: true });
-    router.route('/').get(UserController.getUser);
+    router.route('/')
+      .get(UserController.getUser)
+      .post(UserController.addUser);
+    router.route('/:id')
+      .get(UserController.getUserById);
     return router;
   };
   router.use('/user', userRouter());
